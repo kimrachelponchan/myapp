5860781a70828af7d4a6190d09a65c551d00c3b8
kimrachelponchan <jgim9182@gmail.com>
2025-11-18 21:27:23 +0900

    useState( 버튼 만들기 )

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
 import './App.css';
+import {useState} from "react";
 
-function Circle(props){
-
-  const {color, number} = props;
-
+function Button({color, name, index, setClicked}) {
   return(
-    <div className={`rounded-full h-20 w-20 ${color} flex items-center justify-center`}>{number}</div>
+    <>
+      <button
+        onClick={()=>{setClicked(index)}}
+        className={`${color} button-style`}>나는 {name}색
+      </button>
+    </>
   )
 }
 
-function Box(props){
-
-  const {color, number} = props;
-
-  return(
-    <div className={`h-20 w-20 ${color} flex justify-center items-center`}>{number}</div>
-  )
-}
 
 
 function App() {
+  const [clicked, setClicked] = useState(5);
 
-  const colors = ['bg-red-100','bg-red-200','bg-red-300','bg-red-400','bg-red-500'];
-
+  const color = ['bg-blue-400', 'bg-gray-400', 'bg-red-400', 'bg-green-400', 'bg-yellow-400'];
+  const name = ['파랑', '회색', '빨강', '초록', '노랑', '기본'];
   return(
     <>
-    {colors.map((color, index) =>{
+      <div className={`h-[20vh] w-full`}>
+        <div className={`flex`}>
+          {color.map((colors, index) => {
+            return(
+              <Button color={colors} name={name[index]} index={index} setClicked={setClicked}/>
+            )
+          })}
+        </div>
+        {clicked === 5 ?
+          <div>기본창입니다. 여기는 기본적인 설명이 포함되어 있습니다</div> :
+          <div>{name[clicked]}색을 설명하는 글입니다 여기에는 {name[clicked]}색이 포함되어 있습니다. </div>  
+        }
 
-      const circleNumber = index * 2 + 1;
-      const boxNumber = index * 2 + 2;
-
-      return(
-        <>
-        <Circle color={color} number={circleNumber}/>
-        <Box color={color} number={boxNumber} />
-        </>
-      )
-    }
-  )}
+        {/* {clicked === 1 ?
+          <div>기본창입니다. 여기는 기본적인 설명이 포함되어 있습니다</div> :
+          clicked === 2 ?
+          <div>파랑색을 설명하는 글입니다 여기에는 파랑색이 포함되어 있습니다. </div>:
+          clicked === 3 ?
+          <div>회색색을 설명하는 글입니다 여기에는 회색색이 포함되어 있습니다. </div>:
+          null 
+        } */}
+      </div>
     </>
   )
 }
