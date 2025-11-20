2b956c95c33ecf78b30545d8017b6be1f67f3572
kimrachelponchan <jgim9182@gmail.com>
2025-11-20 20:11:39 +0900

    useRef 스크롤 조작 2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
 import './App.css';
-import {useRef} from 'react';
-// 이 기능을 위해서는 npm install react-router-dom 을 설치해야함함
+import { useRef } from "react";
 
 function App() {
-  
   const boxRef = useRef();
 
   const scrollToBottom = () => {
-    boxRef.current.scrollTop = boxRef.current.scrollHeight;
+    boxRef.current.scrollTo({
+      top: boxRef.current.scrollHeight, // 맨 아래
+      behavior: "smooth"                // 부드럽게 스크롤
+    });
   };
 
   return (
     <div>
-      <div ref={boxRef} style={{ height: "100px", overflowY: "scroll", border: "1px solid black" }}>
-        {Array.from({ length: 50 }, (_, i) => <p key={i}>Item {i+1}</p>)}
+      <div
+        ref={boxRef}
+        style={{
+          height: "100px",
+          overflowY: "scroll",
+          border: "1px solid black"
+        }}
+      >
+        {Array.from({ length: 50 }, (_, i) => (
+          <p key={i}>Item {i + 1}</p>
+        ))}
       </div>
-      <button onClick={scrollToBottom}>맨 아래로</button>
+      <button
+        onClick={scrollToBottom}
+        className="mt-2 px-4 py-1 bg-blue-500 text-white rounded hover:bg-blue-400"
+      >
+        맨 아래로
+      </button>
     </div>
   );
 }
