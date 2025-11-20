db7c38b6255b8c3e270500d6ae40ff545db9db0e
kimrachelponchan <jgim9182@gmail.com>
2025-11-20 20:41:29 +0900

    useRef Array.from 설명 추가가

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       top: boxRef.current.scrollHeight, // 맨 아래
       behavior: "smooth"                // 부드럽게 스크롤
     });
+    console.log(boxRef);
   };
 
   return (
@@ -21,9 +22,13 @@ function App() {
           border: "1px solid black"
         }}
       >
+        {/*Array.from(길이, 값) <- 문법이 이렇게 됌 */}
+        {/* Array.from({length: 30}) -> [udefind...50개] */}
+        
         {Array.from({ length: 50 }, (_, i) => (
           <p key={i}>Item {i + 1}</p>
         ))}
+
       </div>
       <button
         onClick={scrollToBottom}
@@ -36,3 +41,26 @@ function App() {
 }
 
 export default App;
+
+
+// 파이썬, 자바, 자바스크립트 기본 클래스 문법
+
+// class Person:
+//           def __init__(self,name, age):
+//             self.name = name
+//             self.age = age
+
+//           def say(self):
+//             print(self.name);
+          
+//           def modify(self):
+//             self.name = ""
+//             self.age = ""
+
+//           def ageUp(self):
+//             self.age = self.age + 1
+
+//         a = Person("Tom",20)
+//         a.say()
+//         a.modify()
+//         a.ageUp()
