101c707c341b7b2fa002b218c408f37aa8344117
kimrachelponchan <jgim9182@gmail.com>
2025-11-20 18:02:57 +0900

    BrowserRouter, Routes, Route, Link 학습

--- src/App.js
+++ src/App.js
@@ -1,35 +1,70 @@
 import './App.css';
 import {useState} from 'react';
+import {Home} from "./home";
+// 이 기능을 위해서는 npm install react-router-dom 을 설치해야함함
+import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 
-function Box(props){
-
-  const {color, number,count, setCount} = props;
-  let {index} = props;
-
+const Other = ()=>{
   return(
-    // 개별 count 
-    <div onClick={()=> {setCount((prev) => {
-      const newCounts = [...prev];
-      newCounts[index] = newCounts[index] + 1;
-      return newCounts;
-    })}} 
-    // count 1 -> color[0] > color[1](1%3=1)   count 2 -> color[1] > color[2](2%3=2)   count 3 -> color[2] > color[3](3%3=0) count%color.length
-    className={`${color[count%color.length]} h-20 w-20 mx-1`}>나는{number}번째 클릭 수: {count}</div>
+    <>
+      <div>Other</div>
+      <Back/>
+    </>
   )
 }
 
+function A(){
+  return(
+    <>
+      <div>A</div>
+      <Back/>
+    </>
+  )
+}
+
+
+function B(){
+  return(
+    <>
+      <div>B</div>
+      <Back/>
+    </>
+  )
+}
+
+
+function C(){
+  return(
+    <>
+      <div>C</div>
+      <Back/>
+    </>
+  )
+}
+
+function Back(){
+  return(
+    <Link to={`/`}><button className={`button-style2`}>뒤로가기</button></Link>
+  )
+}
+
+
 function App() {
-  const [count, setCount] = useState([0,0,0]);
-  const color = ['bg-red-300','bg-blue-300','bg-yellow-300'];
+  
 
   return (
-    <div className='flex'>
-      {color.map((_, index)=>{
-        return(
-        <Box color={color} number={index + 1} setCount={setCount} count={count[index]} index={index} />
-      )
-      })}
-    </div>
+    // 제일먼저 BrowserRouter로 감쌈
+    <BrowserRouter>
+      {/* 그 다음으로 Routes로 감싸기 */}
+      <Routes>
+        {/* 여기서 부터가 페이지를 나누는 공간 */}
+        <Route path = "/" element={<Home/>}/>
+        <Route path = "/other" element={<Other/>}/>
+        <Route path = "/A" element={<A/>}/>
+        <Route path = "/B" element={<B/>}/>
+        <Route path = "/C" element={<C/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
